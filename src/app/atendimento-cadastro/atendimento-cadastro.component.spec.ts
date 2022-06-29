d5bc9a351efdcd5aa6bcfd4af5897a654f3d9548
smendescoti <sergio.coti@gmail.com>
2022-06-28 21:50:02 -0300

    first commit

---
+++ src/app/atendimento-cadastro/atendimento-cadastro.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AtendimentoCadastroComponent } from './atendimento-cadastro.component';
+
+describe('AtendimentoCadastroComponent', () => {
+  let component: AtendimentoCadastroComponent;
+  let fixture: ComponentFixture<AtendimentoCadastroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AtendimentoCadastroComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AtendimentoCadastroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
